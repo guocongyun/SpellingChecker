ef4dd338972f05523cb68989faa87a7cc0175b77
ASDF1473 <@y49112cg>
2019-12-10 15:01:16 +0000

    Now Generate some text adventure story

--- tkinter_game.py
+++ tkinter_game.py
@@ -40,6 +40,14 @@ def generate_decisions(scenario):
                                    fill="white",
                                    font="Times 10", text=choices[scenario][i]))
 
+def generating_story(image_file, text):
+    canvas.create_text(window_width / 2 + h, h, fill="white", font="Times 10 bold", text=text, anchor=NW)
+    image = PhotoImage(file=image_file)
+    image = image.subsample(3)
+    # scale_w = new_width / old_width
+    # scale_h = new_height / old_height
+    # photoImg.zoom(scale_w, scale_h)
+    canvas.create_image(h, h, image=image, anchor=NW)
 def character_creation():
     # players
     xy[0].append([2 * w, 15 * h, 6 * w, 21 * h])
@@ -110,4 +118,6 @@ characters_size = [
 window, window_width, window_height = configuring_window()
 unit_width, unit_height, w, h = defining_units()
 canvas = configuring_canvas()
+
+generating_story("forest.png", "please select your character")
 window.mainloop()
